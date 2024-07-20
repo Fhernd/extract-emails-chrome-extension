56a46aa87b4607baba5f5e3dcff98c100d948d8b
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-20 14:54:02 -0500

    Configuración de VueJS

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    popup: {
+      entry: 'src/popup.js',
+      template: 'public/popup.html',
+      filename: 'popup.html'
+    }
+  },
+  filenameHashing: false
+};
