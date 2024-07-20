1886acc411896fe2aba03adc13d7f8900056a494
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-20 15:27:57 -0500

    Cambio del ID del componente Popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="principal">
     <h1>Emails Extractor</h1>
   </div>
 </template>
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <style scoped>
-#app {
+#principal {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
