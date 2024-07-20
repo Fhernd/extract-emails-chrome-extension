00afacaef0acbddfa02b97c1505f691fc83e87eb
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-07-20 15:16:28 -0500

    Ajuste componente Popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,23 +1,22 @@
 <template>
-    <div id="app">
-      <h1>Emails Extractor</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "PopupComponent"
-  };
-  </script>
-  
-  <style scoped>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  </style>
-  
+  <div id="app">
+    <h1>Emails Extractor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopupComponent"
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
